47116a9bb03ebbe73cc2792693203e353a3eccc7
Laima <leelalaim@gmail.com>
2023-03-13 15:49:25 +0100

    Remove unused code

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,5 @@
-import { TriangleUpIcon } from '@chakra-ui/icons'
 import styled from 'styled-components'
-import { List, ListItem } from '@chakra-ui/react'
-
-const history = [
-  { year: '2023/01', position: 'Frontend Developer (HiQ)' },
-  { year: '2021/11 - 2022/12', position: 'Frontend Developer (Werlabs)' },
-  {
-    year: '2021/01 - 2021/06',
-    position: 'Frontend Web Development bootcamp (Technigo)',
-  },
-  { year: '2018/05 - 2019/09', position: 'Marketing Manager (Medexy)' },
-  { year: '2018/02 - 2019/05', position: 'Marketing Assistant (Medexy)' },
-  {
-    year: '2014/09 - 2017/08',
-    position: 'BSc Renewable Energy Systems (University of South Wales)',
-  },
-]
+import { List } from '@chakra-ui/react'
 
 const StyledList = styled(List)`
   width: 100%;
